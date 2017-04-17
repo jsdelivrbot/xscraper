bd61e46295747d27ea8e2957ba3034bbd3e4c204
Troy Do <hoangdov@gmail.com>
2017-04-16 22:00:48 -0700

    add media directory

--- example_project/open_news/views.py
+++ example_project/open_news/views.py
@@ -3,5 +3,7 @@ from .models import Article
 
 def article_list(request):
 	articles = Article.objects.all()
+	for article in articles:
+		print(article.thumbnail)
 	return render(request, 'vegasliving.html', {"articles": articles})
 
